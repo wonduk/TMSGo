ae272ca8a74550c172709a6665a5caeac5c07d2a
WONDUKSON <sonwonduk1@naver.com>
2025-02-28 10:49:20 +0900

    backend프로젝트 생성

---
+++ tmsgo-backend/src/main/java/com/tmsgo/tmsgo_backend/TmsgoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tmsgo.tmsgo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsgoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsgoBackendApplication.class, args);
+	}
+
+}
---
+++ tmsgo-backend/src/test/java/com/tmsgo/tmsgo_backend/TmsgoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmsgo.tmsgo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsgoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
